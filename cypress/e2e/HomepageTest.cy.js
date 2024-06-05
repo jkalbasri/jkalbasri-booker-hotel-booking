596965b50b363b0cc7f4e8e7d4f7a5047e17a6e3
albasri <albasri@gmail.com>
2024-06-05 02:38:01 +0200

    redesigning the management system v2.1

--- cypress/e2e/HomepageTest.cy.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="Cypress" />
-
-describe('Homepage', () => {
-  it('Clicking popular destinations cards navigate user to /hotels route', () => {
-    cy.visit('http://localhost:3000/')
-    cy.get('[data-testid=image-card]').first().click()
-    cy.url().should('eq', 'http://localhost:3000/hotels')
-  })
-})
